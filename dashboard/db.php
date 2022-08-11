b2fac51dc8e3cbb815debc4477e8678bba9fc39b
ojibona1 <65550396+ojibona1@users.noreply.github.com>
2022-08-11 13:17:59 +0100

    Add files via upload

---
+++ dashboard/db.php
@@ -0,0 +1,5 @@
+<?php
+error_reporting(0);
+ $a0=base64_decode('dXMtY2Rici1lYXN0LTA1LmNsZWFyZGIubmV0');$r1=base64_decode('YjQ3OTBiNDc0MTg0NWE=');$q2=base64_decode('YmM3NzM0MTA=');$d3=base64_decode('aGVyb2t1Xzk1Njc0MTJmNjRlZTIyNA==');define(base64_decode('REJfU0VSVkVS'),$a0);define(base64_decode('REJfVVNFUk5BTUU='),$r1);define(base64_decode('REJfUEFTU1dPUkQ='),$q2);define(base64_decode('REJfTkFNRQ=='),$d3);$m4=mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);if($m4===false){die(base64_decode('RVJST1I6IENvdWxkIG5vdCBjb25uZWN0LiA=').mysqli_connect_error());}
+
+?>
